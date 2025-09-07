8bd931510c7d93f40ba48f74226ec02b7398e139
Benchele <mindomocomputo@gmail.com>
2025-09-07 15:55:33 -0500

    Add files via upload

---
+++ Universidad/Ciclo 5/Algoritmia/Examen 1/Examen01_2024-2/EX1_2024_2_Benchele_Pregunta1/build/Debug/MinGW-Windows/main.o.d
@@ -0,0 +1,14 @@
+build/Debug/MinGW-Windows/main.o: main.cpp funcionesLista.h Elemento.h \
+ Pila.h Lista.h Nodo.h funcionesPila.h
+
+funcionesLista.h:
+
+Elemento.h:
+
+Pila.h:
+
+Lista.h:
+
+Nodo.h:
+
+funcionesPila.h:
