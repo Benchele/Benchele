0f7940c4c271f71e6b24b7384d9a25ff0b8f2b45
Benchele <mindomocomputo@gmail.com>
2025-09-07 15:59:12 -0500

    Add files via upload

---
+++ Universidad/Ciclo 5/Algoritmia/Examen 2/Examen02_2024-1/EX2_2024_1_Benchele/build/Debug/MinGW-Windows/FuncionesAuxiliares.o.d
@@ -0,0 +1,32 @@
+build/Debug/MinGW-Windows/FuncionesAuxiliares.o: FuncionesAuxiliares.cpp \
+ FuncionesAuxiliares.h funcionesABB.h ArbolBB.h ArbolB.h NodoArbol.h \
+ Elemento.h Cola.h Lista.h Nodo.h funcionesCola.h Pila.h funcionesPila.h \
+ funcionesAB.h funcionesLista.h
+
+FuncionesAuxiliares.h:
+
+funcionesABB.h:
+
+ArbolBB.h:
+
+ArbolB.h:
+
+NodoArbol.h:
+
+Elemento.h:
+
+Cola.h:
+
+Lista.h:
+
+Nodo.h:
+
+funcionesCola.h:
+
+Pila.h:
+
+funcionesPila.h:
+
+funcionesAB.h:
+
+funcionesLista.h:
