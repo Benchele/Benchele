e1e1bbc9b7f78b24abf6b866542f679266551d29
Benchele <mindomocomputo@gmail.com>
2025-09-06 23:09:12 -0500

    Add files via upload

---
+++ Universidad/Ciclo 5/Programacion 2/Laboratorio 8/Laboratorio10_2023-1/Lab10_2023_1_Benchele/build/Debug/MinGW-Windows/Tesoreria.o.d
@@ -0,0 +1,12 @@
+build/Debug/MinGW-Windows/Tesoreria.o: Tesoreria.cpp Tesoreria.h Boleta.h \
+ Alumno.h Escala.h Arbol.h
+
+Tesoreria.h:
+
+Boleta.h:
+
+Alumno.h:
+
+Escala.h:
+
+Arbol.h:
---
+++ Universidad/Ciclo 5/Programacion 2/Laboratorio 8/Laboratorio10_2023-1/Lab10_2023_1_Benchele/build/Debug/MinGW-Windows/main.o.d
@@ -0,0 +1,12 @@
+build/Debug/MinGW-Windows/main.o: main.cpp Tesoreria.h Boleta.h Alumno.h \
+ Escala.h Arbol.h
+
+Tesoreria.h:
+
+Boleta.h:
+
+Alumno.h:
+
+Escala.h:
+
+Arbol.h:
