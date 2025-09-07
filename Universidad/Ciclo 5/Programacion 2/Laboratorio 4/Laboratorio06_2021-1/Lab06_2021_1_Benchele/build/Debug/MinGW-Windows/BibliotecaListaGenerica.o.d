21d562439bbfb2de141bafe0157b016d9cf35bcd
Benchele <mindomocomputo@gmail.com>
2025-09-06 22:48:41 -0500

    Add files via upload

---
+++ Universidad/Ciclo 5/Programacion 2/Laboratorio 4/Laboratorio06_2021-1/Lab06_2021_1_Benchele/build/Debug/MinGW-Windows/BibliotecaListaGenerica.o.d
@@ -0,0 +1,6 @@
+build/Debug/MinGW-Windows/BibliotecaListaGenerica.o: \
+ BibliotecaListaGenerica.cpp FuncionesComunes.h BibliotecaListaGenerica.h
+
+FuncionesComunes.h:
+
+BibliotecaListaGenerica.h:
---
+++ Universidad/Ciclo 5/Programacion 2/Laboratorio 4/Laboratorio06_2021-1/Lab06_2021_1_Benchele/build/Debug/MinGW-Windows/ListaConEnteros.o.d
@@ -0,0 +1,6 @@
+build/Debug/MinGW-Windows/ListaConEnteros.o: ListaConEnteros.cpp \
+ FuncionesComunes.h ListaConEnteros.h
+
+FuncionesComunes.h:
+
+ListaConEnteros.h:
