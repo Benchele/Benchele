fd851ed6ba6aa70f5eadb7769b8aae29fc5d1421
Benchele <mindomocomputo@gmail.com>
2025-09-06 23:38:19 -0500

    Add files via upload

---
+++ Universidad/Ciclo 5/Algoritmia/Laboratorio 3/Examen01_2024-1/EX1_2024_1_Benchele_Pregunta2/build/Debug/MinGW-Windows/_ext/91eac8ec/funcionesLista.o.d
@@ -0,0 +1,12 @@
+build/Debug/MinGW-Windows/_ext/91eac8ec/funcionesLista.o: \
+ ../EX1_2023_1_Benchele/funcionesLista.cpp ../EX1_2023_1_Benchele/Lista.h \
+ ../EX1_2023_1_Benchele/Nodo.h ../EX1_2023_1_Benchele/Elemento.h \
+ ../EX1_2023_1_Benchele/funcionesLista.h
+
+../EX1_2023_1_Benchele/Lista.h:
+
+../EX1_2023_1_Benchele/Nodo.h:
+
+../EX1_2023_1_Benchele/Elemento.h:
+
+../EX1_2023_1_Benchele/funcionesLista.h:
---
+++ Universidad/Ciclo 5/Algoritmia/Laboratorio 3/Examen01_2024-1/EX1_2024_1_Benchele_Pregunta2/build/Debug/MinGW-Windows/_ext/91eac8ec/funcionesPila.o.d
@@ -0,0 +1,18 @@
+build/Debug/MinGW-Windows/_ext/91eac8ec/funcionesPila.o: \
+ ../EX1_2023_1_Benchele/funcionesPila.cpp \
+ ../EX1_2023_1_Benchele/Elemento.h ../EX1_2023_1_Benchele/Pila.h \
+ ../EX1_2023_1_Benchele/Lista.h ../EX1_2023_1_Benchele/Nodo.h \
+ ../EX1_2023_1_Benchele/funcionesPila.h \
+ ../EX1_2023_1_Benchele/funcionesLista.h
+
+../EX1_2023_1_Benchele/Elemento.h:
+
+../EX1_2023_1_Benchele/Pila.h:
+
+../EX1_2023_1_Benchele/Lista.h:
+
+../EX1_2023_1_Benchele/Nodo.h:
+
+../EX1_2023_1_Benchele/funcionesPila.h:
+
+../EX1_2023_1_Benchele/funcionesLista.h:
